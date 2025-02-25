7771cd106568e2597af57e572d6804ab83af0d9e
bunyodzaripov <zaripovbunyod571@gmail.com>
2025-02-25 21:06:25 +0500

    added header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import Search from "./components/search";
+
 const App = () => {
+   const [searchTerm, setSearchTerm] = useState<string>("");
    return (
       <>
-         <h1>Hello</h1>
+         <div className="pattern"></div>
+
+         <div className="wrapper">
+            <header>
+               <h1>
+                  <img src="../public/hero.png" alt="hero" />
+                  Find <span className="text-gradient">Movies</span> You'll
+                  Enjoy without The Hassle
+               </h1>
+               <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+               <p className="text-white">{searchTerm}</p>
+            </header>
+         </div>
       </>
    );
 };
---
+++ src/components/search.tsx
@@ -0,0 +1,22 @@
+interface IndexProps {
+   searchTerm: string;
+   setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Index = ({ searchTerm, setSearchTerm }: IndexProps) => {
+   return (
+      <div className="search">
+         <div>
+            <img src="../../public/search.svg" alt="search" />
+            <input
+               type="text"
+               value={searchTerm}
+               onChange={(e) => setSearchTerm(e.target.value)}
+               placeholder="Search Movies..."
+            />
+         </div>
+      </div>
+   );
+};
+
+export default Index;
